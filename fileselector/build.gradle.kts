a3ef4632eb74ee80993e930bf7b268e1f65bfbb4
satyam <loyltwo3ks.developers@gmail.com>
2026-01-06 10:39:30 +0530

    issue fixing done by satyam

--- fileselector/build.gradle.kts
+++ fileselector/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
-    id ("kotlin-android-extensions")
     id("maven-publish")
 }
 
@@ -44,11 +43,11 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
